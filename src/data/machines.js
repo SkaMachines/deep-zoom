ea01933e6af9e92bfa503b6bdb9f6a2a6204a877
SkaMachines <skamachines@gmail.com>
2026-09-17 14:39:42 -0300

    Create machines.js

---
+++ src/data/machines.js
@@ -0,0 +1,51 @@
+export const machines = {
+  SCS01: {
+    id: "SCS01",
+    name: "Academia Central",
+    location: "",
+    image: "/img/machines/scs01.webp",
+    slots: {
+      "01": {
+        productId: ""
+      },
+      "02": {
+        productId: ""
+      },
+      "03": {
+        productId: ""
+      }
+    }
+  },
+
+  MAUA01: {
+    id: "MAUA01",
+    name: "",
+    location: "",
+    image: "/img/machines/maua01.webp",
+    slots: {}
+  },
+
+  PMFC01: {
+    id: "PMFC01",
+    name: "Clube Primeiro de Maio",
+    location: "Santo André",
+    image: "/img/machines/pmfc01.webp",
+    slots: {}
+  }
+};
+
+NÃO coloque preço aqui.
+
+Cada posição deve apenas apontar para um productId.
+
+Exemplo:
+
+"52": {
+  productId: "MONSTER-ULTRA-473ML"
+}
+
+Se uma posição estiver vazia:
+
+"53": {
+  productId: null
+}
