0b34d5f1f9bc406bcd90cafea8da43d41cdfe25f
SkaMachines <skamachines@gmail.com>
2026-09-17 14:40:40 -0300

    Add product data for Monster Ultra

---
+++ src/data/products.js
@@ -0,0 +1,13 @@
+{
+  id: "MONSTER-ULTRA-473ML",
+  name: "Monster Ultra",
+  brand: "Monster",
+  category: "Energia",
+  volume: "473 ml",
+  description: "",
+  benefits: [],
+  specifications: {},
+  image: "",
+  gallery: [],
+  available: true
+}
