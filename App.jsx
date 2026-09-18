d1d3f620681cebbc469e81fdb34030716ecf2a52
SkaMachines <skamachines@gmail.com>
2026-09-18 08:19:00 -0300

    Update image source in DeepZoom component

--- App.jsx
+++ App.jsx
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div style={{ width: '100vw', height: '100vh', margin: 0, padding: 0 }}>
       {/* O path agora está correto: /public/images/DELTAFIT.jpeg */}
-      <DeepZoom machineImage="/images/DELTAFIT.jpeg" />
+      <DeepZoom machineImage="public/images/SCS.jpeg" />
     </div>
   );
 }
