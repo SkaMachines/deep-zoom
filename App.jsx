9508a62d06b3a7fcee73b75f267fc0193163bf21
SkaMachines <skamachines@gmail.com>
2026-09-18 08:19:33 -0300

    Update image path in App component

--- App.jsx
+++ App.jsx
@@ -4,7 +4,7 @@ import './src/styles/deep-zoom.css';
 export default function App() {
   return (
     <div style={{ width: '100vw', height: '100vh', margin: 0, padding: 0 }}>
-      {/* O path agora está correto: /public/images/DELTAFIT.jpeg */}
+      {/* O path agora está correto: /public/images/SCS.jpeg */}
       <DeepZoom machineImage="public/images/SCS.jpeg" />
     </div>
   );
